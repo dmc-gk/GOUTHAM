b5f88ea3487cbcf6bcfa8e0c5a308cad2486f402
DINUTH978 <sfsafsdfsdfsfdsfds@gmail.com>
2024-08-09 18:44:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "DINU BOY",
+    ownernumber: process.env.Owner_Number || "94781306128",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
