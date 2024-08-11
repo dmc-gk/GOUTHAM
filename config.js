b170b4ce78917875e7fb695d6fe0233fe31e67c1
DINUTH978 <sfsafsdfsdfsfdsfds@gmail.com>
2024-08-11 06:56:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://dinuboy:dinuthyt@cluster0.b05wuue.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "cTR2YWZ_AN_YA_4NDA=",
 
     /**
      * thumbnails and medias
